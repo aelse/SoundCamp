5122e646f30a4c9ccc6bf17b260322681b681964
Alexander Else <alexander.else@team.telstra.com>
2012-02-15 12:17:34 +1100

    sound command will play a sound request noticed in chat

--- sound.js
+++ sound.js
@@ -3,6 +3,7 @@ var sounds = {
 }
 
 var audio = null;
+var re = /:soundcamp \w+/;
 
 function tellPlaySound(sound) {
     var input = $('#input')[0];
@@ -32,12 +33,30 @@ function playSound(sound) {
     audio.play()
 }
 
+function receivedMessage(e) {
+    var t = e.target;
+    if (t.tagName.toLowerCase() === 'tr' && t.id === 'message_pending') {
+        var msgBody = $(t).find('div.body');
+        var msg = msgBody.html();
+        var m = re.exec(msg);
+        if (m != null) {
+            var sound = msg.replace(':soundcamp ', '');
+            playSound(sound);
+        }
+    }
+}
+
 function initControls() {
     $('<div>').attr('id', 'soundcamp_sounds').attr('class', 'tooltip').attr('width', '18px').attr('height', '16px').attr('style', 'margin: 2px 5px;').appendTo('#chat_controls');
     $('<img>').attr('src',
     chrome.extension.getURL('images/music.png')).attr('id', 'soundcamp_button').attr('width', '16').attr('height', '15').appendTo('#soundcamp_sounds');
 
-    $('#soundcamp_button').click(function() { playSound('grenade'); });
+    $('#soundcamp_button').click(function() { tellPlaySound('grenade'); });
+}
+
+function initListener() {
+    $('#chat').bind('DOMNodeInserted', receivedMessage);
 }
 
 initControls();
+initListener();
