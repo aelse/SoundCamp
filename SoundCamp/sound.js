fb53ceab4503310678408b9f4cafd0eab196e99f
David To <david.to@team.telstra.com>
2012-02-16 16:05:00 +1100

    Adding new sounds

--- SoundCamp/sound.js
+++ SoundCamp/sound.js
@@ -15,6 +15,7 @@ var sounds = {
     "jack": ["Jack", "aod-jack.ogg", null, null],
     "nonono": ["No no no...", "no_cat.ogg", "no_cat.png", null],
     "ladiesandgents": ["Steve Ballmer", "steve_ballmer-ladies_and_gentleman.ogg", "steve_ballmer.png", null],
+    "final": ["The Final Countdown", "final.ogg", null, "The Final Countdown" ],
     "timmy": ["Timmy!", "timmy.ogg", "timmy.png", null],
     "right": ["You're right", "chuck_norris-youre_right.ogg", "chuck_norris.png", null],
     "wrong": ["You're wrong", "dr_cox-youre_wrong.ogg", "scrubs.png", null],
