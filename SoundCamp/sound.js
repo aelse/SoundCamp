d55c8ad0bf0f0e6ec773d365864037ffe78bda29
The Franz <mike.franz@hireahelper.com>
2013-07-16 12:03:26 -0700

    Added 4 new clips and reorganized to make it alphabetical to the user.
    We should have it auto sort by display name not key name when we have time.

--- SoundCamp/sound.js
+++ SoundCamp/sound.js
@@ -2,19 +2,16 @@
 var sounds = {
     // key : [ label, soundfile, image, text ]
     "125": ["1, 2, 5!", "1-2-5.ogg", null, null],
-    "goodmorningvietnam": ["Gooood Morning Vietnaaamm!", "good_morning_vietnam.ogg", "good_morning_vietnam.jpg", "Good Morning Vietnam!"],
     "boo": ["ಠ_ಠ", "boohiss.ogg", null, null],
-    "jacksparrow": ["Jack Sparrow", "captain_jack_sparrow.ogg", "captain_jack_sparrow.jpg", "Captain Jack Sparrow!"],
     "alive": ["Alive", "alive.ogg", null, "It's alive!"],
     "alrighty": ["Alrighty then", "alrighty_then.ogg", null, null],
     "america": ["America?", "ThoughtThisWasAmerica.ogg", null, "I am sorry... I thought this was America!!?!"],
     "goat": ["Angry goat", "crazy_goat.ogg", "crazy_goat.png", null],
     "oyster": ["Blue Oyster", "blue_oyster_bar.ogg", "the_blue_oyster.jpg", null],
-    "likeaboss": ["Like A Boss", "like_a_boss.ogg", "like_a_boss.gif", null],
+    "byebyebye": ["Bye Bye Bye", "byebyebye.ogg", "byebyebye.gif", "Bye Bye Bye!"],
     "boomstick": ["Boomstick", "aod-boomstick.ogg", "boomstick.png", null],
     "breakitdown": ["Break it down", "breakitdown.ogg", "McHammer.gif", null],
     "deadbug": ["Bug fixed", "good_bug_dead_bug.ogg", "dead_bug.png", "Bug solved!"],
-    "towel": ["Dont Forget", "DontForgetToBringATowl.ogg", null, null],
     "bug": ["Bug found", "we_have_a_bug.ogg", "bug.png", "We have a bug"],
     "cash": ["Cash", "gimmiecash.ogg", "GimmeDaCash.gif", null],
     "catchaboat": ["Catch a boat", "ricky-catch_a_boat.ogg", "ricky.png", null],
@@ -23,9 +20,12 @@ var sounds = {
     "espresso": ["Coffee, anyone?", "espresso.ogg", "espresso.png", "Anyone for coffee?"],
     "communicate": ["Communicate", "chl-communicate.ogg", null, "What we have here..."],
     "developers": ["Developers...", "steve_ballmer-developers.ogg", "steve_ballmer.png", null],
+    "doOrDoNot": ["Do or Do Not", "DoOrDoNot.ogg", "DoOrDoNot.gif", null],
+    "towel": ["Dont Forget", "DontForgetToBringATowl.ogg", null, null],
     "exterminate": ["Exterminate!", "dalek-exterminate.ogg", "dalek.png", "Exterminate!"],
     "facepalm": ["Facepalm", null, "facepalm.png", "..."],
     "grenade": ["Grenade", "33245__ljudman__grenade.ogg", "grenade.png", "Fire in the hole!"],
+    "goodmorningvietnam": ["Gooood Morning Vietnaaamm!", "good_morning_vietnam.ogg", "good_morning_vietnam.jpg", "Good Morning Vietnam!"],
     "groovy": ["Groovy", "aod-groovy.ogg", null, null],
     "grumpy": ["Grumpy?", "llgrump.ogg", null, null],
     "hailking": ["Hail to the king", "aod-hailking.ogg", "crown.png", null],
@@ -33,10 +33,12 @@ var sounds = {
     "holdon": ["Hold on!", "HoldOnToYourButts.ogg", null, null],
     "hoaah": ["Hoaah", "timmy_hoaah.ogg", "timmy.png", null],
     "hypnotoad": ["Hypnotoad", "hypnotoad.ogg", "hypnotoad.gif", "ALL GLORY TO THE HYPNOTOAD!"],
+    "knowingishalf": ["Knowing is Half", "KnowingIsHalfTheBattle.ogg", "KnowingIsHalfTheBattle.jpg", null],
     "dontthinkso": ["I don't think so", "larry_david-no_i_dont_think_so.ogg", "larry_david.jpg", null],
     "infinity": ["Infinity", "to_inf.ogg", "buzz_lightyear.png", "To infinity! ... or somewhere a bit closer"],
     "trap": ["It's a trap!", null, "star_wars_its_a_trap.png", null],
     "jack": ["Jack", "aod-jack.ogg", null, null],
+    "jacksparrow": ["Jack Sparrow", "captain_jack_sparrow.ogg", "captain_jack_sparrow.jpg", "Captain Jack Sparrow!"],
     "jackpot": ["Jackpot!", "Jackpot.ogg", null, null],
     "jebus": ["Jebus", "jesus_christ.ogg", "mr_slave.png", null],
     "killhumans": ["Kill all humans", "bender_kill_humans.ogg", "bender_kill.png", null],
@@ -44,15 +46,17 @@ var sounds = {
     "leeroy": ["Leeroy", "leeroy_jenkins.ogg", null, "At least I have chicken."],
     "letsgetready": ["Let's Get Ready", "LetsGetReadyToRumble.ogg", null, "Let's get ready to rumble!"],
     "letsgo": ["Let's go", "idiotmbl.ogg", null, null],
+    "likeaboss": ["Like A Boss", "like_a_boss.ogg", "like_a_boss.gif", null],
     "livinalie": ["Livin' a lie", "timmy_livinalie.ogg", "timmy.png", "Timmah!"],
     "needassiatnce": ["Need assistance?", "NeedAssistance.ogg", null, null],
     "nicehat": ["Nice hat", "nicehat.ogg", "NiceHat.gif", null],
     "nonono": ["No no no...", "no_cat.ogg", "no_cat.png", null],
     "notime": ["No time for that", "aint_nobody_got_time.ogg", null, null],
+    "orangeMocha": ["Orange Mocha!", "OrangeMochaFrappuccino.ogg", "OrangeMochaFrappacino.gif", null],
+    "chosepoorly": ["Poor Choice", "HeChosePoorly.ogg", null, null],
     "sad": ["Sad", "sad.ogg", "buzz_lightyear.png", "Sad..."],
     "screwyouguys": ["Screw you guys", "ScrewYouGuys.ogg", null, null],
     "panda": ["Sad Panda", "sad_panda.ogg", "sad_panda.png", null],
-    "chosepoorly": ["Poor Choice", "HeChosePoorly.ogg", null, null],
     "sheeeit": ["Sheeeit", "sheeeit.ogg", "clay_davis.png", null],
     "snozberries": ["Snozberries", "Snozberries.ogg", null, "This snozberries taste like realy snozberries"],
     "sorryiamlate": ["Sorry I'm Late", "SorryIAmLate.ogg", null, "Sorry I am late!"],
@@ -63,18 +67,17 @@ var sounds = {
     "timmy": ["Timmy!", "timmy.ogg", "timmy.png", null],
 	"verbal": ["Verbal morality", "verbal_morality.ogg", "verbal_morality.png", null],
     "what": ["What?", "WhatMinion.ogg", "what.gif", null],
+    "whatisthis": ["What is this?", "WhatIsThis.ogg", "WhatIsThis.jpg", "What is this?"],
     "yeah": ["Yeah, Yeah!", "YeahYeahYeah.ogg", null, "Yeah, Yeah, Yeah, Yeah, Yeah"],
     "youawizard": ["You're a wizard", "WizardHarry.ogg", null, "You're a wizard, Harry.."],
     "yourcrazy": ["You're crazy", "YourCrazy.ogg", null, "I like you... but you're crazy..."],
     "right": ["You're right", "chuck_norris-youre_right.ogg", "chuck_norris.png", null],
     "wrong": ["You're wrong", "dr_cox-youre_wrong.ogg", "scrubs.png", null],
-    "thebest": ["You're the best", "YouAreTheBest.ogg", null, "You're the best!"],
-    "orangeMocha": ["Orange Mocha!", "OrangeMochaFrappuccino.ogg", "OrangeMochaFrappacino.gif", null]
+    "thebest": ["You're the best", "YouAreTheBest.ogg", null, "You're the best!"]
 }
 
 var soundKeys = Object.keys(sounds);
 var soundKeysLength = soundKeys.length;
-soundKeys.sort();
 
 var audio = null;
 var re = /:soundcamp \w+/;
